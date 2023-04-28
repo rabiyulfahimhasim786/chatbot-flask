fd3a56b15e88ed47165b1a1da7ff0a4d321e18ec
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-04-28 05:30:41 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    # TODO: check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    datas = str(get_response(userText))
+    # message = [{'data': datas}]
+    # return str(get_response(userText))
+    # return jsonify({'response': message})
+    return jsonify({'response': datas})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
